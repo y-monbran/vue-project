536577936475abfebc543e1198f11aed9690f377
y-monbran <gohangaoisiiyo@gmail.com>
2020-08-28 17:35:39 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,46 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>    
+    <svg>
+      <polygon class="item" v-for="(item, index) in items" :key="index" points="200,300 200,350 400,250" fill="tomato"
+        @click="click($event)"
+      />
+    </svg>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  export default {
+    name: "app",
+    
+    data () {
+      return {
+        
+        
+        items: [
+          { id: 1, name: 'name01' },
+        ],
+        methods: {
+          click: function(e) {
+            console.log("クリックした");
+          }
+        }
+      }
     }
   }
-}
+</script>
+
+<style>
+  svg {
+    height: 1000px;
+    width: 1000px;
+    background-color: lightgray;
+  }
+  .item:hover {
+    cursor: grab;
+  }
+  .item:active {
+    cursor: grabbing;
+  }
 </style>
